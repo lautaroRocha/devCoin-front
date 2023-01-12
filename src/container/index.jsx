b7ba31a3f00887589b8d008d6fcdb74cb4cd3e98
Lautaro Rocha <102425176+lautaroRocha@users.noreply.github.com>
2023-01-12 13:15:07 -0300

    Revert "Lucas branch development"

--- src/container/index.jsx
+++
@@ -1,7 +0,0 @@
-// Pages
-
-// export { default as Home } from './Home/Home';
-// export { default as Wallet } from './Wallet/Wallet';
-// export { default as UserProfile } from './UserProfile/UserProfile';
-// export { default as Settings } from './Settings/Settings';
-// export { default as Footer } from './Footer/Footer';
