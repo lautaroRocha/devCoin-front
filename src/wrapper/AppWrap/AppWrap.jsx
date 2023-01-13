c34c0ff541ea7f1d04f8fbf34a9b530b0530222b
Lucas Samuel Pereyra <65786717+lucaspereyradev@users.noreply.github.com>
2023-01-13 12:15:58 -0300

    Wrapper terminado.

--- src/wrapper/AppWrap/AppWrap.jsx
+++ src/wrapper/AppWrap/AppWrap.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AppWrap = (Component) =>
     function HOC() {
         return (
-            <div className="w-fullpl-[6rem] float-right h-screen pt-[1.5rem] pb-[1.5rem] pr-[2rem] xl:w-[85%] xl:pl-[2rem] 1700:w-[88%]">
+            <div className="w-full pl-[6rem] float-right h-screen pt-[1.5rem] pb-[1.5rem] pr-[2rem] xl:w-[85%] xl:pl-[2rem] 1700:w-[88%]">
                 <Component />
             </div>
         );
