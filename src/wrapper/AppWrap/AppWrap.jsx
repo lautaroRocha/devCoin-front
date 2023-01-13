afe9bd84fb81147f6630167570f7dd22a13cb6cd
Lucas Samuel Pereyra <65786717+lucaspereyradev@users.noreply.github.com>
2023-01-13 12:25:43 -0300

    Wrapper updated, syntax error fixed.

--- src/wrapper/AppWrap/AppWrap.jsx
+++ src/wrapper/AppWrap/AppWrap.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AppWrap = (Component) =>
     function HOC() {
         return (
-            <div className="w-full pl-[6rem] float-right h-screen pt-[1.5rem] pb-[1.5rem] pr-[2rem] xl:w-[85%] xl:pl-[2rem] 1700:w-[88%]">
+            <div className="float-right h-screen w-full pl-[6rem] pt-[1.5rem] pb-[1.5rem] pr-[2rem] xl:w-[85%] xl:pl-[2rem] 1700:w-[88%]">
                 <Component />
             </div>
         );
