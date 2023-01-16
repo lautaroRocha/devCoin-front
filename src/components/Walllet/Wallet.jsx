0116111cea40ee6724d1f62fef4dbbed25046d47
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-16 09:49:09 -0300

    fxed balance responsive

--- src/components/Walllet/Wallet.jsx
+++ src/components/Walllet/Wallet.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {Balance} from '../../components'
 
 const Wallet = () => {
+
+    const [transactionsByUser, setTransactionsByUser] = useState([])
+    const [transactionsToUser, setTransactionsToUser] = useState([])
+
+
     return (
-        <>
+        <div className='flex flex-col h-screen items-center  w-11/12 ml-auto lg:w-8/12 lg:mr-auto lg:ml-45'>
         <h1>WALLET</h1>
             <Balance />
             <h2>Tus monedas</h2>
@@ -14,7 +19,7 @@ const Wallet = () => {
             <h3>Transferencias</h3>
                 <span>Historial</span>
                 <span>Enviar</span>
-        </>
+        </div>
     );
 }
 
