3b75e3f93af5e0d3dc6ee4dd5111935f6b96faa0
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-17 12:17:27 -0300

    color indicator in wallet view

--- src/components/Walllet/Wallet.jsx
+++ src/components/Walllet/Wallet.jsx
@@ -23,15 +23,26 @@ const Wallet = () => {
             view = <Transfer/>
             break;
     }
+
+        const sectionButtons = document.querySelectorAll('#li')
+        sectionButtons.forEach( (btn) =>{
+            if(btn.textContent === selectedView){
+                btn.style.backgroundColor = "#f5061d"
+            }else{
+                btn.style.backgroundColor =  '#c0012a'
+            }
+        })
+ 
+
     return (
         <div className='flex flex-col items-center h-full w-full'>
         <h1 className='mt-6 text-6xl text-primary font-bold'>WALLET</h1>
         <Deposit />
         <button className='bg-alternative w-fit mx-auto p-2 rounded-lg mt-3 font-semibold hover:bg-alternative/80'>COMPRAR</button>
             <ul className='flex gap-6 text-white  text-sm font-bold mt-4'>
-                <li className='bg-secondary dark:bg-secodary/90 p-2 rounded-lg hover:cursor-pointer' onClick={setView}>Balance</li>
-                <li className='bg-secondary dark:bg-secodary/90 p-2 rounded-lg hover:cursor-pointer' onClick={setView}>Historial</li>
-                <li className='bg-secondary dark:bg-secodary/90 p-2 rounded-lg hover:cursor-pointer' onClick={setView}>Enviar</li>
+                <li className='bg-secondary dark:bg-secodary/90 p-2 rounded-lg hover:cursor-pointer hover:bg-secondary/80' onClick={setView} id="li" >Balance</li>
+                <li className='bg-secondary dark:bg-secodary/90 p-2 rounded-lg hover:cursor-pointer hover:bg-secondary/80' onClick={setView} id="li" >Historial</li>
+                <li className='bg-secondary dark:bg-secodary/90 p-2 rounded-lg hover:cursor-pointer hover:bg-secondary/80' onClick={setView} id="li" >Enviar</li>
             </ul>
             {view}
         </div>
