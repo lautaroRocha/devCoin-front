f74274b7128494c777d4f5d78e54bf5cedfc53df
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-18 10:17:32 -0300

    gonzo changes

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -42,6 +42,14 @@ const LoginPage = () => {
                             />
                         </div>
                         <button className="home-buttons mt-4 w-full">Ingresar</button>
+                        <div className="flex w-full">
+                            <input id="remember" type='checkbox'></input>   
+                            <label for='remember' className='focus:outline-none dark:bg-black/90 dark:text-white'>Recordarme</label>                            
+                        </div>                                
+                        <button className="home-buttons mt-4 w-full">Ingresar</button>  
+                        <div className="flex w-full">
+                            <a href="#" className='text-center hover:underline'>Restablecer Password?</a>
+                        </div> 
                     </form>
                 </div>
             </div>
