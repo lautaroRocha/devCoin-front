9576ff0516ceb7a670cac6537b3bdefb59313f4c
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-18 13:31:36 -0300

    register user

---
+++ src/utils/URL.js
@@ -0,0 +1,14 @@
+export const url = "https://devcoin-production.up.railway.app"
+
+export const users = url + "/user"
+
+export const transaction = url + "/transaction"
+
+export const wallet = url + "/wallet"
+
+export const coins = url + "/coins"
+
+export const login = users + "/logIn"
+
+export const recover = users + "/recover"
+
