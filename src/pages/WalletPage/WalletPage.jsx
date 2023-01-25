744511a0161a0b79ae7c44137c1faf4c6e7e84f5
Lautaro Rocha <102425176+lautaroRocha@users.noreply.github.com>
2023-01-25 17:38:59 -0300

    Revert "Fix / balance css"

--- src/pages/WalletPage/WalletPage.jsx
+++ src/pages/WalletPage/WalletPage.jsx
@@ -41,27 +41,26 @@ const Wallet = (props) => {
         return (
             <>
                 <h1 className="mr-auto text-2xl font-bold">Wallet</h1>
-                <div className="flex w-full flex-col items-center gap-8 overflow-hidden lg:grid lg:h-[90vh] lg:grid-cols-4 lg:grid-rows-4 lg:gap-9">
-
+                <div className="flex w-full flex-col items-center gap-8 overflow-hidden lg:grid lg:h-[70vh] lg:grid-cols-[420px,_1fr] lg:grid-rows-4 lg:gap-x-5">
                     <Deposit update={props.props.update} />
-                    <div className="lg:items-left flex flex-col items-center lg:col-start-1 lg:col-end-4 lg:row-start-2 gap-8 lg:mt-16">
-                         <ul className="mt-4 flex gap-6  text-sm font-bold text-white h-3/12">
-                             <li
-                                 className="dark:bg-secodary/90 rounded-sm bg-secondary p-2 hover:cursor-pointer"
-                                 onClick={setView}
-                                 id="view-btn"
-                             >
-                                 Balance
-                             </li>
-                             <li
-                                 className="dark:bg-secodary/90 rounded-sm bg-secondary p-2 hover:cursor-pointer"
-                                 onClick={setView}
-                                 id="view-btn"
-                             >
-                                 Historial
-                             </li>
-                         </ul>
-                         {view}
+                    <div className="lg:items-left flex h-[50vh] flex-col items-center lg:col-start-1 lg:col-end-4 lg:row-start-2">
+                        <ul className="mt-4 flex gap-6  text-sm font-bold text-white ">
+                            <li
+                                className="dark:bg-secodary/90 rounded-sm bg-secondary p-2 hover:cursor-pointer"
+                                onClick={setView}
+                                id="view-btn"
+                            >
+                                Balance
+                            </li>
+                            <li
+                                className="dark:bg-secodary/90 rounded-sm bg-secondary p-2 hover:cursor-pointer"
+                                onClick={setView}
+                                id="view-btn"
+                            >
+                                Historial
+                            </li>
+                        </ul>
+                        {view}
                     </div>
                     <Transfer className="lg:col-start-2" />
                 </div>
