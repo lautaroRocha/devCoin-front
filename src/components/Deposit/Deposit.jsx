0d1c9f1b705548302afe7f0e8f8b1a0f730017d7
Lucas Samuel Pereyra <lucaspereyra470@gmail.com>
2023-01-25 17:16:15 -0300

    Deposit and Transfer styles updated

--- src/components/Deposit/Deposit.jsx
+++ src/components/Deposit/Deposit.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { toast } from 'react-toastify';
 import * as URL from '../../utils/URL';
 import { userContext, tokenContext, walletContext } from '../../context';
@@ -7,6 +7,8 @@ const Deposit = (props) => {
     const wallet = useContext(walletContext);
     const user = useContext(userContext);
     const token = useContext(tokenContext);
+
+    const [valueToDeposit, setValueToDeposit] = useState('');
 
     const depositFiatMoney = (obj) => {
         fetch(URL.wallet + '/' + user.hex_code, {
@@ -25,8 +27,7 @@ const Deposit = (props) => {
     };
 
     const handleDeposit = (e) => {
-        e.preventDefault;
-        let valueToDeposit = prompt('¿Cuántos USD quieres depositar?');
+        e.preventDefault();
         if (valueToDeposit) {
             let depositData = {
                 balance: parseFloat(valueToDeposit),
@@ -38,14 +39,35 @@ const Deposit = (props) => {
     };
 
     return (
-        <div className="h-18 mt-6 grid w-full grid-cols-2 items-center rounded-lg bg-secondary/50 p-2 text-center font-semibold lg:col-start-4">
-            <span className="m-auto text-center">Tu saldo actual es de ${wallet.balance}</span>
-            <button
-                className="m-auto w-fit rounded-lg bg-alternative p-2 hover:bg-alternative/80"
-                onClick={handleDeposit}
-            >
-                DEPOSITAR
-            </button>
+        <div className="max-lg:w-full lg:col-start-4 lg:grid">
+            <div className="mt-6 flex h-[18rem] w-full flex-col items-center rounded-md bg-secondary px-6 text-center text-white shadow-md 306:h-[16rem] 316:h-[14rem] 500:h-[12.5rem] 600:h-[10rem] 600:flex-row lg:gap-x-4">
+                <h4 className="flex w-full flex-col items-center gap-y-2 text-center font-semibold max-600:justify-center max-600:gap-x-4 max-600:py-6 500:flex-row 600:flex-col">
+                    Balance actual: <span className="text-2xl">${wallet.balance} USD </span>
+                </h4>
+                <div className="flex w-full flex-col gap-y-4">
+                    <input
+                        type="number"
+                        placeholder="¿Cuanto sumas?"
+                        className="w-full rounded-md py-2 px-4 text-black focus:outline-none"
+                        onChange={(e) => setValueToDeposit(e.target.value)}
+                    />
+                    {valueToDeposit === '' ? (
+                        <button
+                            className="w-full overflow-hidden overflow-ellipsis whitespace-nowrap rounded-md bg-slate-400 p-2 px-0 font-semibold transition-colors"
+                            disabled
+                        >
+                            Depositar
+                        </button>
+                    ) : (
+                        <button
+                            className="w-full overflow-hidden overflow-ellipsis whitespace-nowrap rounded-md bg-green-500 p-2 px-0 font-semibold transition-colors hover:bg-green-500/80"
+                            onClick={handleDeposit}
+                        >
+                            Depositar
+                        </button>
+                    )}
+                </div>
+            </div>
         </div>
     );
 };
