c231d445f970d50e448e327a7371f30a63e5eb85
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-30 14:04:45 -0300

    minCoin link fixed

--- src/components/MinCoin/MinCoin.jsx
+++ src/components/MinCoin/MinCoin.jsx
@@ -5,6 +5,7 @@ import { coinsContext } from '../../context';
 const MinCoin = ({ coin, index }) => {
     const { prices } = useContext(coinsContext);
     let coinCurrentPrice = prices.find((prices) => prices.name === coin.name);
+    const nameForLink = coin.name.toLowerCase()
 
     return (
         <tr
@@ -12,7 +13,7 @@ const MinCoin = ({ coin, index }) => {
             className="border-b bg-white text-black/90 dark:border-gray-700 dark:bg-neutral-800/20 dark:text-white"
         >
             <td className="flex items-center gap-3 py-4 pl-6">
-                <Link to={`/coins/${coin.id}`} className="flex items-center gap-3 py-4 pl-6">
+                <Link to={`/coins/${nameForLink}`} className="flex items-center gap-3 py-4 pl-6">
                     <img className="min-w-[25px] max-w-[30px]" src={coin.image} alt={coin.name} />
                     <span className="font-bold uppercase  dark:text-white">{coin.symbol}</span>
                     <span className="text-[#707a8a] dark:text-[848e9c]">{coin.name}</span>
