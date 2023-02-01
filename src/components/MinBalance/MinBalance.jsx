5af5d8b544b8b2cab136ea2a87ad2e41e129d1fd
Lucas Samuel Pereyra <lucaspereyra470@gmail.com>
2023-02-01 11:42:19 -0300

    Wrapper fixed, and some changes added

--- src/components/MinBalance/MinBalance.jsx
+++ src/components/MinBalance/MinBalance.jsx
@@ -1,19 +1,16 @@
-import React, {useContext} from 'react';
-import * as Icons from "../../utils/icons"
+import React, { useContext } from 'react';
+import * as Icons from '../../utils/icons';
 import { coinsContext } from '../../context';
 
-const MinBalance = ( ) => {
-
-    const {wallet} = useContext(coinsContext)
+const MinBalance = () => {
+    const { wallet } = useContext(coinsContext);
 
     return (
-        <div className="md:glassMoney mt-4 flex h-[4rem] w-full items-center justify-start gap-x-4 rounded-lg bg-gray-900/90 px-[4rem] dark:bg-gray-800/50 dark:text-white max-350:px-[0.5rem] md:ml-5 md:mt-0 md:h-full md:w-[40%]">
-        <span>{Icons.bill}</span>
-        <span className="ml-auto text-2xl font-bold lg:text-4xl">
-            ${wallet.balance}
-        </span>
-    </div>
+        <div className="glassMoney mt-4 flex h-[4rem] w-full items-center justify-start gap-x-4 rounded-md px-[4rem] dark:bg-gray-800/50 dark:text-white max-350:px-[0.5rem] md:ml-5 md:mt-0 md:h-[11.5rem] md:w-[40%]">
+            <span>{Icons.bill}</span>
+            <span className="ml-auto text-2xl font-bold lg:text-4xl">${wallet.balance}</span>
+        </div>
     );
-}
+};
 
 export default MinBalance;
